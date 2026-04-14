b331c6faa1581bc9cac1d6caf231ef4e57317e00
OReactko <son682014@gmail.com>
2026-04-14 16:50:30 +0700

    Change shell

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.shell = "pwsh"
+vim.opt.shell = "zsh"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 vim.opt.fileencodings = { "utf-8", "ucs-bom", "utf-16", "latin1", "cp932", "sjis" }
